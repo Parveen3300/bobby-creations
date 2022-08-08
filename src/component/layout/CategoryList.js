bf27da26c093bb4d49ef74c77dbeb4746017c5bb
Parveen3300 <parveenprajapati@loopmethods.com>
2022-08-08 18:22:24 +0530

    home screen Apis

--- src/component/layout/CategoryList.js
+++ src/component/layout/CategoryList.js
@@ -1,22 +1,26 @@
-import React from 'react';
-import Categories from './Homepage.json';
-import { Carousel, Image, Typography, Card } from 'antd';
+import React from "react";
+import Categories from "../layout/Homepage.json";
+import { Carousel, Image, Typography, Card } from "antd";
 const { Title } = Typography;
 const CategoryList = () => {
-    return (
-        <>
-            <Title level={4} className='mb-3'>Tops-Tees</Title>
-            <Carousel arrows slidesToShow={6}>
-                {
-                    Categories.Categories.map((data) => {
-                        return (<><Card className='mx-2'><Image src={data.category_img} />
-                            <h6 className='mt-3'>{data.category_name}</h6></Card>
-                        </>)
-                    })
-                }
-            </Carousel>
-
-        </>
-    )
-}
-export default CategoryList
+  return (
+    <>
+      <Title level={4} className="mb-3">
+        Tops-Tees
+      </Title>
+      <Carousel arrows slidesToShow={6}>
+        {Categories.Categories.map((data) => {
+          return (
+            <>
+              <Card className="mx-2">
+                <Image src={data.category_img} />
+                <h6 className="mt-3">{data.category_name}</h6>
+              </Card>
+            </>
+          );
+        })}
+      </Carousel>
+    </>
+  );
+};
+export default CategoryList;
--- src/component/layout/Home.js
+++ src/component/layout/Home.js
@@ -9,21 +9,27 @@ import {
   Col,
   Tabs,
   Card,
+  Divider,
+  Space,
 } from "antd";
 import Header from "./Header";
 import "../../component/form.scss";
 import { homepage } from "../../services/masterData";
 import CategoryList from "./CategoryList";
-// // import { MenuOutlined } from "@ant-design/icons";
+import { StarOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 // import Banners from "./Homepage.json";
 // import { Link } from "react-router-dom";
 // import ProductCard from "./ProductCard";
-const { TabPane } = Tabs;
-// const { Header } = Layout;
 const { Title } = Typography;
 const Home = () => {
   const [banner, setbanner] = useState([]);
-  const [categories, setCategories] = useState([{}]);
+  const [categories, setCategories] = useState([]);
+  const [shopCategories, setShopCategories] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [bottoms, setBottoms] = useState([]);
+  const [summerdress, setSummerdress] = useState([]);
+  const [womenshirts, setWomenshirts] = useState([]);
 
   useEffect(() => {
     homepage({
@@ -35,10 +41,19 @@ const Home = () => {
       .then((response) => {
         if (response.status === 200) {
           setbanner(response.data.home_screen_service.new_banners);
-          setCategories(response.data.home_screen_service.mode_products?.[0].products_data[0]?.product_image)
-            console.log(
-            "mode_name",
-            response.data.home_screen_service?.mode_products?.[0].products_data[0]?.product_image
+          setCategories(
+            response.data.home_screen_service.mode_products?.[0].products_data
+          );
+          setTitle(response.data.home_screen_service);
+          setBottoms(
+            response.data.home_screen_service.mode_products?.[1].products_data
+          );
+          setShopCategories(response.data.home_screen_service.mode_products);
+          setSummerdress(
+            response.data.home_screen_service.mode_products?.[2].products_data
+          );
+          setWomenshirts(
+            response.data.home_screen_service.mode_products?.[3].products_data
           );
         }
       })
@@ -50,7 +65,6 @@ const Home = () => {
   // let imagepath = Banners.Banners;
   return (
     <div className="container-fluid">
-
       <Header />
       <div className="banner">
         <Carousel autoplay arrows effect="fade" className="h-75">
@@ -62,30 +76,198 @@ const Home = () => {
           ))}
         </Carousel>
       </div>
+
+      <div className="mt-5 h-50 container" style={{ padding: "0px 12px" }}>
+        <Title level={4} className="my-5">
+          Shop By Categories
+        </Title>
+        <Carousel arrows autoplay slidesToShow={4}>
+          {shopCategories.map((data) => {
+            return (
+              <>
+                <Link
+                  to="/all-categories"
+                  className="center category"
+                  style={{ display: "block" }}
+                >
+                  <Image
+                    src={
+                      "http://172.104.186.221:8000" +
+                      data.products_data?.[2].product_image?.[0].short_image
+                    }
+                    preview={false}
+                    className="short_image"
+                  />
+                  <h6 className="mt-4">
+                    {data.products_data?.[0].product_modes?.[0].mode_name}
+                  </h6>
+                </Link>
+              </>
+            );
+          })}
+        </Carousel>
+      </div>
+
       <div className="container">
         {/* <Divider /> */}
 
-        <Row gutter={36}>
-        <Col md={24}>
-        <Carousel arrows slidesToShow={6}>
-                 {
-                    categories.map((data,i) => {
-                        return (<><div className='mx-2'>
-                            {/* <h6 className='mt-3'>{data.mode_name}</h6> */}
-                            <Image src={"http://172.104.186.221:8000" + data.short_image} preview={false}/>
-                            </div>
-                        </>)
-                    })
-                } 
+        <Row gutter={36} className="mt-5">
+          <Col md={24}>
+            <Title level={4} className="mb-3">
+              {
+                title.mode_products?.[0].products_data?.[0].product_modes?.[0]
+                  .mode_name
+              }
+            </Title>
+            <Carousel arrows autoplay slidesToShow={6}>
+              {categories.map((data, i) => {
+                return (
+                  <>
+                    <Card className="mx-2 my-2 text-center">
+                      <div className="product_image">
+                        <Image
+                          src={
+                            "http://172.104.186.221:8000" +
+                            data.product_image?.[0].short_image
+                          }
+                          preview={false}
+                          className="short_image"
+                        />
+                      </div>
+                      <h6 className="mt-3 fs-12">{data.product_name}</h6>
+                      <Divider />
+                      <div className="d-flex justify-content-between mx-2">
+                        <StarOutlined style={{ fontSize: "24" }} />
+                        <ShoppingCartOutlined style={{ fontSize: "24" }} />
+                      </div>
+                    </Card>
+                  </>
+                );
+              })}
             </Carousel>
-        </Col>
-       </Row> 
+          </Col>
+        </Row>
 
+        <div className="bottoms">
+          <Row gutter={36} className="mt-5">
+            <Col md={24}>
+              <Title level={4} className="mb-3">
+                {
+                  title.mode_products?.[1].products_data?.[0].product_modes?.[0]
+                    .mode_name
+                }
+              </Title>
+              <Carousel arrows autoplay slidesToShow={6}>
+                {bottoms.map((data, i) => {
+                  return (
+                    <>
+                      <Card className="mx-2 my-2 text-center">
+                        <div className="product_image">
+                          <Image
+                            src={
+                              "http://172.104.186.221:8000" +
+                              data.product_image?.[0].short_image
+                            }
+                            preview={false}
+                            className="short_image"
+                          />
+                        </div>
+                        <h6 className="mt-3 fs-12">{data.product_name}</h6>
+                        <Divider />
+                        <div className="d-flex justify-content-between mx-2">
+                          <StarOutlined style={{ fontSize: "24" }} />
+                          <ShoppingCartOutlined style={{ fontSize: "24" }} />
+                        </div>
+                      </Card>
+                    </>
+                  );
+                })}
+              </Carousel>
+            </Col>
+          </Row>
+        </div>
 
+        <div className="Women-shirts">
+          <Row gutter={36} className="mt-5">
+            <Col md={24}>
+              <Title level={4} className="mb-3">
+                {
+                  title.mode_products?.[3].products_data?.[0].product_modes?.[0]
+                    .mode_name
+                }
+              </Title>
+              <Carousel arrows autoplay slidesToShow={6}>
+                {womenshirts.map((data) => {
+                  return (
+                    <>
+                      <Card className="mx-2 my-2 text-center">
+                        <div className="product_image">
+                          <Image
+                            src={
+                              "http://172.104.186.221:8000" +
+                              data.product_image?.[0].short_image
+                            }
+                            preview={false}
+                            className="short_image"
+                          />
+                        </div>
+                        <h6 className="mt-3 fs-12">{data.product_name}</h6>
+                        <Divider />
+                        <div className="d-flex justify-content-between mx-2">
+                          <StarOutlined style={{ fontSize: "24" }} />
+                          <ShoppingCartOutlined style={{ fontSize: "24" }} />
+                        </div>
+                      </Card>
+                    </>
+                  );
+                })}
+              </Carousel>
+            </Col>
+          </Row>
+        </div>
 
-         <div className="my-5 ">
-          <CategoryList />
-        </div> 
+        <div className="summer-dress">
+          <Row gutter={36} className="mt-5">
+            <Col md={24}>
+              <Title level={4} className="mb-3">
+                {
+                  title.mode_products?.[2].products_data?.[0].product_modes?.[0]
+                    .mode_name
+                }
+              </Title>
+              {/* <Carousel arrows autoplay slidesToShow={6}> */}
+              <Space>
+                {summerdress.map((data, i) => {
+                  return (
+                    <>
+                      <Card className="mx-2 my-2 text-center">
+                        <div className="product_image">
+                          <Image
+                            src={
+                              "http://172.104.186.221:8000" +
+                              data.product_image?.[0].short_image
+                            }
+                            preview={false}
+                            className="short_image"
+                          />
+                        </div>
+                        <h6 className="mt-3 fs-12">{data.product_name}</h6>
+                        <Divider />
+                        <div className="d-flex justify-content-between mx-2">
+                          <StarOutlined style={{ fontSize: "24" }} />
+                          <ShoppingCartOutlined style={{ fontSize: "24" }} />
+                        </div>
+                      </Card>
+                    </>
+                  );
+                })}
+                {/* </Carousel> */}
+              </Space>
+            </Col>
+          </Row>
+        </div>
+
+        <div className="my-5 ">{/* <CategoryList /> */}</div>
       </div>
     </div>
   );
