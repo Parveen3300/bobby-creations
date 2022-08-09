6eb15acd2ab85845031514fba98ce6b0fc85f62f
Parveen Prajapati <parveenprajapati@loopmethods.com>
2022-08-09 22:37:57 +0530

    home

--- src/component/layout/all-categories/Bottom.js
+++ src/component/layout/all-categories/Bottom.js
@@ -5,6 +5,7 @@ import Header from "../Header";
 import { StarOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import "../../../component/layout/layout.scss";
 // import { Link } from "react-router-dom";
+
 const Bottom = () => {
   const [bottoms, setBottoms] = useState([]);
   const [title, setTitle] = useState([]);
@@ -33,37 +34,39 @@ const Bottom = () => {
     <div className="DesinerDress">
       <Header />
 
-      <div className="bottoms">
+      <div className="bottoms container mt-5">
         <Title level={4} className="mb-3">
           {
             title.mode_products?.[1].products_data?.[0].product_modes?.[0]
               .mode_name
           }
         </Title>
-        {bottoms.map((data, i) => {
-          return (
-            <>
-              <Card className="mx-2 my-2 text-center">
-                <div className="product_image">
-                  <Image
-                    src={
-                      "http://172.104.186.221:8000" +
-                      data.product_image?.[0].short_image
-                    }
-                    preview={false}
-                    className="short_image"
-                  />
-                </div>
-                <h6 className="mt-3 fs-12">{data.product_name}</h6>
-                <Divider />
-                <div className="d-flex justify-content-between mx-2">
-                  <StarOutlined style={{ fontSize: "24" }} />
-                  <ShoppingCartOutlined style={{ fontSize: "24" }} />
-                </div>
-              </Card>
-            </>
-          );
-        })}
+        <div className="dashboard-card">
+          {bottoms.map((data, i) => {
+            return (
+              <>
+                <Card className="mx-2 my-2 text-center">
+                  <div className="product_image">
+                    <Image
+                      src={
+                        "http://172.104.186.221:8000" +
+                        data.product_image?.[0].short_image
+                      }
+                      preview={false}
+                      className="short_image"
+                    />
+                  </div>
+                  <h6 className="mt-3 fs-12">{data.product_name}</h6>
+                  <Divider />
+                  <div className="d-flex justify-content-between mx-2">
+                    <StarOutlined style={{ fontSize: "24" }} />
+                    <ShoppingCartOutlined style={{ fontSize: "24" }} />
+                  </div>
+                </Card>
+              </>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
